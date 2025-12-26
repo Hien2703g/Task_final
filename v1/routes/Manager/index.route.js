c5724d07676bd4575cec6ab73eed9d1c8a5bcb84
Hiền Trần <ht800423@gmail.com>
2025-12-26 16:50:13 +0700

    dashboard

--- v1/routes/Manager/index.route.js
+++ v1/routes/Manager/index.route.js
@@ -1,5 +1,6 @@
 const projectRoute = require("./project.route");
 const userRoute = require("./user.route");
+const dashboardRoute = require("./dashboard.route");
 
 const authMiddleware = require("../../middlewares/Manager/auth.middlewares");
 const settingMiddleware = require("../../middlewares/Manager/setting.middleware");
@@ -8,6 +9,8 @@ module.exports = (app) => {
   app.use(settingMiddleware.settingGeneral);
 
   app.use(version + "/projects", authMiddleware.requireAuth, projectRoute);
+
+  app.use(version + "/dashboard", authMiddleware.requireAuth, dashboardRoute);
 
   app.use(version + "/users", userRoute);
 };
